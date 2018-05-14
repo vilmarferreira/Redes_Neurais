9b51c514e79f6378d0e3b20a0f53ee2d51b2d5ed
Vilmar Ferreira <ferreiragomes7792@gmail.com>
2018-05-14 08:26:33 -0300

    implementação completa

--- MultiCamada/rede_multiCamada.py
+++ MultiCamada/rede_multiCamada.py
@@ -1,20 +1,74 @@
 import numpy as np
 
-def sigmoid (soma):
-    return 1/(1 + np.exp(-soma))
 
-entradas = np.array([[0,0], [0,1],[1,0],[1,1]])
-saidas = np.array([[0],[1],[1],[0]])
+def sigmoid(soma):
+    return 1 / (1 + np.exp(-soma))
 
-pesos0 = np.array([-0.424, -0.740, -0.961], [0.358, -0.577, -0.469]) #pesos para x1 e x2 ou seja peso para os neuronios
 
-pesos1= np.array([[-0.017], [-0.893][0.148]])
+def sigmoidDerivada(sig):
+    return sig * (1 - sig)
 
-epocas = 100 #rodadas de vezes em que vai rodar (fazer os ajustes) trein time
 
-for j in range (epocas):
+# a = sigmoid(0.5)
+# b = sigmoidDerivada(a)
+
+# a = sigmoid(-1.5)
+# b = np.exp(0)
+
+entradas = np.array([[0, 0],
+                     [0, 1],
+                     [1, 0],
+                     [1, 1]])
+
+saidas = np.array([[0], [1], [1], [0]])
+
+# pesos0 = np.array([[-0.424, -0.740, -0.961],
+#                   [0.358, -0.577, -0.469]])
+
+# pesos1 = np.array([[-0.017], [-0.893], [0.148]])
+
+pesos0 = 2 * np.random.random((2, 3)) - 1
+pesos1 = 2 * np.random.random((3, 1)) - 1
+
+epocas = 10000000
+taxaAprendizagem = 0.6
+momento = 1
+
+for j in range(epocas):
     camadaEntrada = entradas
-    somaSinapse0 = np.dot(camadaEntrada,pesos0)
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
     camadaOculta = sigmoid(somaSinapse0)
+
     somaSinapse1 = np.dot(camadaOculta, pesos1)
     camadaSaida = sigmoid(somaSinapse1)
+
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    print("Erro: " + str(mediaAbsoluta))
+
+
+    derivadaSaida = sigmoidDerivada(camadaSaida)
+    deltaSaida = erroCamadaSaida * derivadaSaida
+
+    pesos1Transposta = pesos1.T
+    deltaSaidaXPeso = deltaSaida.dot(pesos1Transposta)
+    deltaCamadaOculta = deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
+
+    camadaOcultaTransposta = camadaOculta.T
+    pesosNovo1 = camadaOcultaTransposta.dot(deltaSaida)
+    pesos1 = (pesos1 * momento) + (pesosNovo1 * taxaAprendizagem)
+
+    camadaEntradaTransposta = camadaEntrada.T
+    pesosNovo0 = camadaEntradaTransposta.dot(deltaCamadaOculta)
+    pesos0 = (pesos0 * momento) + (pesosNovo0 * taxaAprendizagem)
+
+    # print(erroCamadaSaida)
+    # print(mediaAbsoluta)
+    # print("delta saida ")
+    # print(deltaSaida)
+    # print("pesos1 ")
+    # print(pesos1)
+    #
+    # print("pesos novos0 ")
+    # print(pesosNovo0)
+print(camadaSaida)
